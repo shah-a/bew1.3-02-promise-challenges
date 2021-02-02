9d03a5e14334917d42d3d70ec8135cbdb32f3eb1
Ali Shah <s.ashah@outlook.com>
2021-02-02 04:00:18 -0500

    [Finished] challenge 1

--- challenges/challenge1.js
+++ challenges/challenge1.js
@@ -6,14 +6,28 @@
  * 1. Read over the code that follows. In what order will the outputs "Step 1",
  *    "Step 2", and "Step 3" be printed? How do you know?
  * 
+ *    The order will be "Step 1", "Step 3", "Step 2". "setTimeout()" uses the
+ *    event-loop to execute a function asynchronously after some milliseconds.
+ *    In the meantime, the code moves on to the next line (which, in this case,
+ *    is "Step 3").
  * 
  * 2. Run this code using `node challenge1.js`. In what order were the steps
  *    printed?
  * 
+ *    Step 1, Step 3, Step 2 -- as expected.
  * 
  * 3. Change the delay time in the `snooze` function from 2000 ms to 0. In what
  *    order will the steps be printed now? Why? Re-run the code again to verify
  *    your expectation. Were you correct?
+ * 
+ *    My guess is that order will not change. The reason why I think this is
+ *    because even though there's a theoretical 0 millisecond delay, I think
+ *    invoking setTimeout() would send the call to the event-loop which would
+ *    take some amount of time, even if that time is close to 0.
+ * 
+ *    After running the code, it still ran in the same order as before. But I'm
+ *    not sure if it's because of the reason I wrote above or because of another
+ *    reason.
  * 
  *******************************************************************************
  */
@@ -22,7 +36,7 @@
 function snooze(action) {
   setTimeout(function () {
     action();
-  }, 2000);
+  }, 0);
 }
 console.log('Step 1');
 
